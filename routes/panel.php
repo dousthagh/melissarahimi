99ba355e86edf13bf065702c7e18953046feac4a
dousthagh <mohammad.dousthagh@gmail.com>
2023-12-26 18:24:43 +0330

    user arvan video

--- routes/panel.php
+++ routes/panel.php
@@ -49,7 +49,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get("user_terms", [UserController::class, "UserTerms"])->name("user.terms");
         Route::get("accept_user_terms", [UserController::class, "AcceptUserTerms"])->name("user.terms.accept");
     });
-    
+
 
     Route::get('/dashboard', function () {
         if (auth()->user()->accept_terms)
@@ -141,6 +141,7 @@ Route::middleware(['auth'])->group(function () {
             Route::middleware(['auth.student'])->group(function () {
                 Route::get("/{user_level_category_id}", [LessonController::class, "GetLessonList"])->name("user_level_category.lesson.list");
                 Route::get("details/{userLevelCategoryId}/{lessonId}", [LessonController::class, "GetLessonDetails"])->name("user_level_category.lesson.details");
+                Route::get("show_video/{video_id}", [LessonController::class, "ShowVideo"])->name("user_level_category.lesson.video.show");
                 Route::get("get_address/{key}/{userLevelCategoryId}/{private_key}", [LessonController::class, "GetLessonFileAddressBySecretKey"])->name("user_level_category.lesson.files.address");
                 Route::post("send_sample_work", [LessonController::class, "SendSampleWork"])->name("user_level_category.lesson.sample_work.send");
                 Route::get("sample_work/{lessonId}/{userLevelCategoryId}", [LessonController::class, "MySampleWorkList"])->name("user_level_category.lesson.sample_work");
